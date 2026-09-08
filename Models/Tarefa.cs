f76de60c5cbc3a9a14276d270ac667c543f962e0
juliocesar-dev-br <jc309681@gmail.com>
2026-09-08 19:54:23 -0300

    feat: add task model

---
+++ Models/Tarefa.cs
@@ -0,0 +1,34 @@
+public class Tarefa
+{
+    public int Id {get; private set;}
+    public string Titulo {get; set;} = string.Empty;
+    public string Descricao {get; set;} = string.Empty;
+    public bool estaConcluida {get; private set;} = false;
+
+
+
+    public Tarefa(int id, string titulo, string descricao)
+    {
+        Id = id;
+        Titulo = titulo;
+        Descricao = descricao;
+    }
+
+    public override string ToString()
+    {
+        return $"Id: {Id} | Titulo: {Titulo} | Descrição: {Descricao} | Concluída: {estaConcluida}";
+    }
+
+    public string ConcluirTarefa()
+    {
+        if(estaConcluida)
+        {
+            return $"A tarefa: {Titulo} já se encontra concluída!";
+        }
+       
+        estaConcluida = true;
+        return $"A tarefa: {Titulo} foi conclúida com sucesso!";
+    }
+
+
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Tarefa tarefaTeste = new(1, "Alistamento no exercito", "Fazer o alistamento obrigatório no exercito pois eu acabei de completar 18 anos.");
+
+Console.WriteLine(tarefaTeste);
+
+Console.WriteLine(tarefaTeste.ConcluirTarefa());
+
+Console.WriteLine(tarefaTeste);
+
+Console.WriteLine(tarefaTeste.ConcluirTarefa());
